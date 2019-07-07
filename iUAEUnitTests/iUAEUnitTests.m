918b2609aea1d970194fc2b35be4251a52aba97c
Simon <simontoens@gmail.com>
2019-07-07 15:27:52 +0900

    Add new test target

---
+++ iUAEUnitTests/iUAEUnitTests.m
@@ -0,0 +1,36 @@
+//
+//  iUAEUnitTests.m
+//  iUAEUnitTests
+//
+//  Created by Simon Toens on 7/7/19.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iUAEUnitTests : XCTestCase
+
+@end
+
+@implementation iUAEUnitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
